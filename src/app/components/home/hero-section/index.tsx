30711bdff8fb1728d93bd9ee443c5ed210a4ac1c
Ashfaq Mansuri <ashfaqmn37@gmail.com>
2025-11-11 22:55:32 +0530

    Initial portfolio upload

---
+++ src/app/components/home/hero-section/index.tsx
@@ -0,0 +1,55 @@
+import { getImgPath } from "@/utils/image";
+import Image from "next/image";
+
+const index = () => {
+  return (
+    <section className="relative hero-section overflow-hidden pt-35 md:pt-40 pb-12 lg:pb-30 xl:pt-52">
+      <div className="container">
+        <div className="lg:flex grid grid-cols-1 sm:grid-cols-2 gap-7 md:gap-4 items-center">
+          <div className="flex flex-col gap-4 md:gap-7 max-w-2xl">
+            <div>
+              <div className="flex items-center gap-8">
+                <h3>I'm Mohammad Ashfaq Mansuri</h3>
+                <div className="wave">
+                  <Image
+                    src={getImgPath("/images/home/banner/wave-icon.svg")}
+                    alt="wave-icon"
+                    width={62}
+                    height={62}
+                    className=""
+                  />
+                </div>
+              </div>
+              <h4>Web Developer & Graphic Designer</h4>
+            </div>
+            <p className="text-secondary font-normal max-w-md xl:max-w-xl">
+              To leverage my skills in web development, graphic design,
+              multimedia content creation, and Microsoft Office tools (Excel,
+              PowerPoint, and Word) to contribute to innovative and creative
+              projects. I aim to enhance my technical expertise while delivering
+              high-quality results in a dynamic and collaborative environment.
+            </p>
+          </div>
+          <Image
+            src={getImgPath("/images/home/banner/banner-img.png")}
+            alt="banner-img"
+            width={685}
+            height={650}
+            className="block lg:hidden"
+          />
+        </div>
+      </div>
+      <div className="absolute right-0 top-0 hidden h-auto w-1/2 lg:block 2xl:h-171.5 2xl:w-187.5">
+        <Image
+          src={getImgPath("/images/home/banner/banner-img.png")}
+          alt="banner-img"
+          width={685}
+          height={650}
+          className=" absolute right-0 top-0 z-1"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default index;
